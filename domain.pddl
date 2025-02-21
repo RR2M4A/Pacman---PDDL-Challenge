d4207392ee638d5fac69d8e3261147f76bb151a8
Jose <josevictor21112005@gmail.com>
2025-02-20 22:53:59 -0300

    Criação da movimentação do pacman sobre células livres

--- domain.pddl
+++ domain.pddl
@@ -5,10 +5,88 @@
         celula criatura
     )
     (:predicates
+        
+        (turno-pacman)
 
+        (criatura-em ?c1 - criatura ?c2 - celula)
+        (celula-direita ?c1 ?direita-de-c1 - celula)
+        (celula-esquerda ?c1 ?esquerda-de-c1 - celula)
+        (celula-cima ?c1 ?cima-de-c1 - celula)
+        (celula-baixo ?c1 ?baixo-de-c1 - celula)
+
+        ; Tipos de células
+        (eh-parede ?c - celula)
+        (eh-chao ?c - celula)
     )
     
     (:constants 
         pacman fantasma-verde fantasma-vermelho fantasma-azul - criatura
     )
+
+    ; MOVIMENTAÇÃO PACMAN
+
+    ; Direita
+    (:action DIREITA
+        :parameters (?c1 ?c2 - celula)
+        
+        :precondition (and 
+            (turno-pacman)
+            (celula-direita ?c1 ?c2)
+            (criatura-em pacman ?c1)
+            (eh-chao ?c2)
+        )
+        :effect (and            
+            (not (criatura-em pacman ?c1))
+            (criatura-em pacman ?c2)
+        )
+    )
+
+    ; Esquerda
+    (:action ESQUERDA-W
+        :parameters (?c1 ?c2 - celula)
+        
+        :precondition (and 
+            (turno-pacman)
+            (celula-esquerda ?c1 ?c2)
+            (criatura-em pacman ?c1)
+            (eh-chao ?c2)
+        )
+        :effect (and            
+            (not (criatura-em pacman ?c1))
+            (criatura-em pacman ?c2)
+        )
+    )
+
+    ; Cima
+    (:action CIMA-N
+        :parameters (?c1 ?c2 - celula)
+        
+        :precondition (and 
+            (turno-pacman)
+            (celula-cima ?c1 ?c2)
+            (criatura-em pacman ?c1)
+            (eh-chao ?c2)
+        )
+        :effect (and            
+            (not (criatura-em pacman ?c1))
+            (criatura-em pacman ?c2)
+        )
+    )
+
+    ; Baixo
+    (:action BAIXO-S
+        :parameters (?c1 ?c2 - celula)
+        
+        :precondition (and 
+            (turno-pacman)
+            (celula-baixo ?c1 ?c2)
+            (criatura-em pacman ?c1)
+            (eh-chao ?c2)
+        )
+        :effect (and            
+            (not (criatura-em pacman ?c1))
+            (criatura-em pacman ?c2)
+        )
+    )
+    
 )
--- parser.py
+++ parser.py
@@ -28,7 +28,96 @@ def get_name(line: str, col: str) -> str:
 
     return f"p_{line}_{col}"
 
+
+def write_problem(board: list):
+
+    objs = write_objs(board)
+    init = write_init(board)
+    goal = write_goal(board)
+
+    new_problem_str = PROBLEM_STR.replace("objects", f"objects \n{objs}")
+    new_problem_str = new_problem_str.replace("init", f"init \n{init}")
+    new_problem_str = new_problem_str.replace("and", f"and \n{goal}")
+
+    with open("problem.pddl", "w") as problem_file:
+        problem_file.write(new_problem_str)
+
+
+def write_objs(board: list) -> str:
+    
+    """Função responsável por escrever o :objects do arquivo de problema."""
+
+    objs = ""
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            objs += f"{get_name(i, j)} "
+
+    objs += "- celula\n"
+    return objs
+
+
+def write_init(board: list) -> str:
+    
+    """Função responsável por escrever o :init do arquivo de problema."""
+
+    init = ""
+    direita = ""
+    esquerda = ""
+    cima = ""
+    baixo = ""
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+
+            current_cell = get_name(i, j)
+
+            # ADJACÊNCIAS
+
+            # Verifica a célula à direita
+            k = j + 1
+            if k < len(board[0]):
+                direita += (f"(celula-direita {current_cell} {get_name(i, k)})\n")
+            
+            # Verificando a célula abaixo
+            k = i + 1
+            if k < len(board):
+                baixo += (f"(celula-baixo {current_cell} {get_name(k, j)})\n")
+
+            # Verificando a célula à esquerda
+            k = j - 1
+            if k >= 0:
+                esquerda += (f"(celula-esquerda {current_cell} {get_name(i, k)})\n")
+
+            # Verificando a célula acima
+            k = i - 1
+            if k >= 0:
+                cima += (f"(celula-cima {current_cell} {get_name(k, j)})\n")
+
+            # SPAWN
+            
+            if board[i][j] == " ":
+                init += f"(eh-chao {current_cell})\n"
+            elif board[i][j] == "#":
+                init += f"(eh-parede {current_cell})\n"
+            elif board[i][j] == "P":
+                init += f"(criatura-em pacman {current_cell})\n"
+            elif board[i][j] == "R":
+                init += f"(criatura-em fantasma-vermelho {current_cell})\n"
+            elif board[i][j] == "B":
+                init += f"(criatura-em fantasma-azul {current_cell})\n"
+            elif board[i][j] == "G":
+                init += f"(criatura-em fantasma-verde {current_cell})\n"
+
+
+    init += "(turno-pacman)\n" + direita + baixo + esquerda + cima
+    return init
+
+
+def write_goal(board: list) -> str:
+    return "(criatura-em pacman p_1_1)\n"
+
 if __name__ == "__main__":
 
     my_board = make_board()
     write_problem(my_board)
+    os.system("./planners/madagascar/Mp -o plan.txt -S 1 domain.pddl problem.pddl")
